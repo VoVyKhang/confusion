80888e3368729ea47a24e7587bc1ffb10db52d08
VoVyKhang <vo.vykhang24@gmail.com>
2022-07-12 14:43:51 +0700

    Redux Actions

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,4 @@
 import {createStore,combineReducers} from 'redux';
-import { Reducer, initialState } from './reducer'
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
