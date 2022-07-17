7635607b8ac95889dd4c7d7991f4def26eac25d1
VoVyKhang <vo.vykhang24@gmail.com>
2022-07-17 22:35:36 +0700

    React Animations

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,12 @@
-import {createStore,combineReducers, applyMiddleware } from 'redux';
+import {createStore, combineReducers, applyMiddleware } from 'redux';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import { createForms } from 'react-redux-form';
 import { InitialFeedback } from './forms';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -19,9 +19,11 @@ export const ConfigureStore = () => {
                 feedback: InitialFeedback
             })
         }),
+
         applyMiddleware(thunk, logger)
     );
-
+    
     return store;
 }
+
 
